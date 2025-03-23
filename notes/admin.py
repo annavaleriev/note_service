87f14e138ff8d4d18036c0c0dfb87c098d2d3247
annavaleriev <filenko.a@gmail.com>
2025-03-23 18:08:29 +0300

    Создана админка для notes

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from notes.models import UserProfile, CarLoanCenter
+from notes.models import UserProfile, CarLoanCenter, Hub, Notes
 
 
 # Register your models here.
@@ -15,3 +15,25 @@ class UserProfileAdmin(admin.ModelAdmin):
 class CarLoanCenterAdmin(admin.ModelAdmin):
     fields = ("name", "hub")
     list_display = ("name", "hub")
+
+@admin.register(Hub)
+class HubAdmin(admin.ModelAdmin):
+    fields = ("name", )
+    list_display = ("name", )
+
+
+@admin.register(Notes)
+class NotesAdmin(admin.ModelAdmin):
+    fields = ("parus_url", "car_loan_center", "created_at", "update_at", "appoval_date", "subject", "owner",
+              "observers", "status")
+    list_display = ("parus_url", "car_loan_center", "created_at", "update_at", "appoval_date", "subject", "owner",
+                    "get_observers", "status")
+    list_filter = ("parus_url", "car_loan_center", "created_at", "update_at", "appoval_date", "subject", "owner",
+                     "status")
+    search_fields = ("car_loan_center", "owner", "created_at", "subject", "status", "update_at", "appoval_date",
+                     "observers", "parus_url")
+
+    def get_observers(self, obj):
+        return ",".join([user.username for user in obj.observers.all()])
+    get_observers.short_description = "Observers"
+
