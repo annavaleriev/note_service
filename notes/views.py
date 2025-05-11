d34ebabd2f09d33fe2af2185e4666781b0b7312f
annavaleriev <filenko.a@gmail.com>
2025-05-11 17:03:03 +0300

    [feature-filters] обновлен permission_classes и class NotesViewSet

--- notes/views.py
+++ notes/views.py
@@ -1,35 +1,61 @@
+from django.db.models import Q
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-from notes.filters import NotesFilter, CarLoanCenterFilter
-from notes.models import CarLoanCenter, Hub, Notes
-from notes.serializer import (
-    CarLoanCenterSerializer,
-    HubSerializer,
-    NotesCreateSerializer,
-    NotesSerializer,
-)
+from notes.filters import CarLoanCenterFilter, NotesFilter, UserProfileFilter
+from notes.models import CarLoanCenter, Hub, Notes, UserProfile
+from notes.permissions import HasUserProfilePermission
+from notes.serializer import (CarLoanCenterSerializer, HubSerializer,
+                              NotesCreateSerializer, NotesSerializer,
+                              PermissionSerializer, UserProfileSerializer)
 
 
-class HubViewSet(
-    viewsets.ReadOnlyModelViewSet
-):
+def check_user_access(user_profile, note):
+    """Проверяет доступ пользователя к записке"""
+
+    if (
+        note.owner != user_profile
+        and not note.observers.filter(id=user_profile.id).exists()
+    ):
+        raise PermissionDenied("У вас нет доступа для записки в этом ЦАК")
+    return True
+
+
+class UserProfileViewSet(mixins.ListModelMixin, GenericViewSet):
+    queryset = UserProfile.objects.all().order_by("car_loan_center")
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated, HasUserProfilePermission]
+    filterset_class = UserProfileFilter
+    filter_backends = [DjangoFilterBackend]
+
+    @action(serializer_class=PermissionSerializer, detail=False)
+    def permissions(self, request):
+        user_profile = self.request.user.userprofile
+        serializer = self.get_serializer(user_profile)
+        return Response(serializer.data)
+
+
+class HubViewSet(viewsets.ReadOnlyModelViewSet):
     """ViewSet для хаба"""
 
     queryset = Hub.objects.all().order_by("name")
     serializer_class = HubSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, HasUserProfilePermission]
+    filter_backends = [DjangoFilterBackend]
 
 
-class CarLoanCenterViewSet(
-    viewsets.ReadOnlyModelViewSet
-):
+class CarLoanCenterViewSet(viewsets.ReadOnlyModelViewSet):
     """ViewSet для центра автокредитования"""
 
     queryset = CarLoanCenter.objects.all().order_by("name")
     serializer_class = CarLoanCenterSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, HasUserProfilePermission]
+    filter_backends = [DjangoFilterBackend]
     filterset_class = CarLoanCenterFilter
 
 
@@ -38,11 +64,23 @@ class NotesViewSet(viewsets.ModelViewSet):
 
     queryset = Notes.objects.all().order_by("subject")
     serializer_class = NotesSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, HasUserProfilePermission]
     filter_backends = [DjangoFilterBackend]
     filterset_class = NotesFilter
 
     def get_serializer_class(self):
+        """Возвращает сериализатор в зависимости от действия"""
+
         if self.action in ("create", "update", "partial_update"):
             return NotesCreateSerializer
         return super().get_serializer_class()
+
+    def get_queryset(self):
+        """Возвращает список записок где пользователь является владельцем или наблюдателем"""
+        queryset = super().get_queryset()
+        user_profile = self.request.user.userprofile
+
+        return queryset.filter(
+            Q(car_loan_center=user_profile.car_loan_center)
+            & (Q(owner=user_profile) | Q(observers=user_profile))
+        )
