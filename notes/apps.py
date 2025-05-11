a656cbaf837de70a9ddcb058ca826490f6828d3b
annavaleriev <filenko.a@gmail.com>
2025-05-11 17:00:42 +0300

    [feature-filters] PEP8

--- notes/apps.py
+++ notes/apps.py
@@ -2,6 +2,8 @@ from django.apps import AppConfig
 
 
 class NotesConfig(AppConfig):
+    """Настройки приложения служебных записок (notes)"""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "notes"
     verbose_name = "служебные записки"
